3c885cf7d40e6ac8e196837c0ec2043dfcfcc5ee
ashr81 <ashrithreddi81@gmail.com>
2015-08-23 22:18:18 +0530

    Total working app

--- app/src/main/java/com/webmanchuria/www/modernartui/MainActivity.java
+++ app/src/main/java/com/webmanchuria/www/modernartui/MainActivity.java
@@ -16,7 +16,6 @@ import android.widget.SeekBar;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-    private int seek11,seek12,seek21,seek22,seek23;
     SeekBar seekBar;
     LinearLayout seekLayout11,seekLayout12,seekLayout21,seekLayout22,seekLayout23;
     int value;
@@ -24,19 +23,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //seekLayout11 = (LinearLayout) findViewById(R.id.L1L1);
+        seekLayout11 = (LinearLayout) findViewById(R.id.L1L1);
         seekLayout12 = (LinearLayout) findViewById(R.id.L1L2);
         seekLayout21 = (LinearLayout) findViewById(R.id.L2L1);
         seekLayout22 = (LinearLayout) findViewById(R.id.L2L2);
         seekLayout23 = (LinearLayout) findViewById(R.id.L2L3);
         seekBar = (SeekBar) findViewById(R.id.seekBar);
-        final TextView tt = (TextView) findViewById(R.id.textView);
         updateBackground();
         seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
             public void onProgressChanged(SeekBar seekBar, int progress,
                                           boolean fromUser) {
-                int value = seekBar.getProgress();
-                tt.setText(Integer.toString(value));
                 updateBackground();
             }
 
@@ -49,22 +45,18 @@ public class MainActivity extends AppCompatActivity {
     }
     protected void updateBackground() {
         value = new Integer(seekBar.getProgress()).intValue();
-        //seekLayout11.setBackgroundColor(0xF8F8FF);
+
         seekLayout12.setBackgroundColor(
-                0x00ff0000
-                        + value<<0x10000
+                0xA352CC + value<<8
         );
         seekLayout21.setBackgroundColor(
-                0xffff0000
-                        + value<<0x10000
+                0xB2B2B2 + value<<8
         );
         seekLayout22.setBackgroundColor(
-                0xff0000ff
-                        + value << 0x1000
+                0xFF7519 + value <<8
         );
         seekLayout23.setBackgroundColor(
-                0x00ffff00
-                        + value << 0x10000
+               0xFF66CC + value <<8
         );
     }
 
@@ -80,11 +72,9 @@ public class MainActivity extends AppCompatActivity {
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
-        final TextView tm = (TextView) findViewById(R.id.textView2);
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
             //return true;
-            tm.setText("Hi Everyone");
             final AlertDialog.Builder alertDialog = new AlertDialog.Builder(this);
             alertDialog.setTitle("This is my Android App developed for Coursera Assessment:");
             alertDialog.setMessage("Click Below to Learn More.");
